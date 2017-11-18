9a6912e1b3649d0fa253c29b25e1341659b53996
Trevor Condon <condontrevor@gmail.com>
2017-11-17 21:19:45 -0700

    import those views. otherwise 404 foreverrr

--- citycompare/__init__.py
+++ citycompare/__init__.py
@@ -10,3 +10,4 @@ flask_app.logger.setLevel(logging.ERROR)
 flask_app.config.from_object('config')
 flask_app.config['SECRET_KEY'] = 'super-secret-key-that-noone-can-guess'
 bootstrap = flask_bootstrap.Bootstrap(flask_app)
+from citycompare import views
