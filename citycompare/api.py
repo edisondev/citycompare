a332f554217bc26245d5361f8f01764e9bcd0d4a
mojtaba-komeili <mojtaba.komeili@gmail.com>
2017-11-18 19:23:06 -0700

    Added api calls that makes the fire dataframes

--- citycompare/api.py
+++ citycompare/api.py
@@ -5,9 +5,9 @@ AIR_QUALITY = 'Air Quality'
 EMERGENCY_RESPONSE = 'Emergency Response'
 
 
-def fetch_results(api, order=None):
+def fetch_results(api, order=None, select=None, where=None, limit=2000):
     client = Socrata(api['root'], None)
-    return client.get(api['code'], order=order, limit=2000)
+    return client.get(api['code'], order=order, select=select, where=where, limit=2000)
 
 def calgary_air_quality(api):
     results = fetch_results(api, order='date DESC')
@@ -28,10 +28,57 @@ def edmonton_air_quality(api):
     return clean_results_df
 
 def calgary_emergency_response(api):
-    pass
+    results = fetch_results(api, select = 'alarm_year,incident_count,major_incident_type',
+                            where = 'alarm_year>2014', limit=10000)
+    df = pd.DataFrame.from_records(results)
+    df['incident_count'] = pd.to_numeric(df['incident_count'])
+    alarm_years = df.alarm_year.unique().tolist()
+    alarm_type  = ['Medical/Rescue', 'Fire'] # Arbitrarily selected. Full list by: df.major_incident_type.unique().tolist()
+
+    year = []
+    inc_type = []
+    count = []
+    for y in alarm_years:
+        for t in alarm_type:
+            year.append(y)
+            inc_type.append(t)
+            df2 = df.loc[(df['alarm_year'] == y) & (df['major_incident_type'] == t)]
+            c = sum(df2['incident_count'].tolist())
+            count.append(c)
+
+    d = {'year': year, 'incident type': inc_type, 'count': count}
+    conc_df = pd.DataFrame(data=d)
+    return  conc_df
+
+
 
 def edmonton_emergency_response(api):
-    pass
+    results = fetch_results(api, select = 'dispatch_year, count, event_description',
+                            where = 'dispatch_year>2014', limit=30000)
+    df = pd.DataFrame.from_records(results)
+    df['count'] = pd.to_numeric(df['count'])
+    df['event_description'] = df['event_description'].replace(
+        ['FIRE', 'VEHICLE FIRE', 'OUTSIDE FIRE', 'PERMIT-BURNING OR OTHER'], 'Fire')
+    df['event_description'] = df['event_description'].replace(['MEDICAL', 'RESCUE'], 'Medical/Rescue')
+
+
+    alarm_years = df.dispatch_year.unique().tolist()
+    alarm_type  = ['Fire', 'Medical/Rescue'] # Arbitrarily selected. Full list by: df.event_description.unique().tolist()
+
+    year = []
+    inc_type = []
+    count = []
+    for y in alarm_years:
+        for t in alarm_type:
+            year.append(y)
+            inc_type.append(t)
+            df2 = df.loc[(df['dispatch_year'] == y) & (df['event_description'] == t)]
+            c = sum(df2['count'].tolist())
+            count.append(c)
+
+    d = {'year': year, 'incident type': inc_type, 'count': count}
+    conc_df = pd.DataFrame(data=d)
+    return  conc_df
 
 
 CITY_DATA_API_MAP = {
@@ -61,8 +108,8 @@ CITY_DATA_API_MAP = {
         },
         EMERGENCY_RESPONSE: {
             'api': {
-                'root': r'data.calgary.ca',
-                'code': r'bdez-pds9'
+                'root': r'data.edmonton.ca',
+                'code': r'hpqc-zvnm'
             },
             'callback': edmonton_emergency_response
         }
